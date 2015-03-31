8dd479cb4a16a41a471cc170b0cb8354a6577b27
Dave Gadsden <davegadsden92@googlemail.com>
2015-04-01 00:15:26 +0100

    Added pictures and skills section

--- app/home/homeController.js
+++ app/home/homeController.js
@@ -4,7 +4,13 @@ var homeController =
     ['$scope',
 
         function($scope) {
-
+            $scope.skills = [
+                { skillname: 'HTML5', img: '/assets/img/html5.png', imagealt: 'HTML5', description: 'Description 1' },
+                { skillname: 'CSS3', img: '/assets/img/css.png', imagealt: 'CSS3', description: 'Description 2' },
+                { skillname: 'JavaScript', img: '/assets/img/js3.png', imagealt: 'JavaScript', description: 'Description 3' },
+                { skillname: 'AngularJS', img: '/assets/img/angularjs.png', imagealt: 'AngularJS', description: 'Description 4' },
+                { skillname: 'Ionic Mobile Hybrid Framework', img: '/assets/img/ionic_logo.png', imagealt: 'Ionic', description: 'Description 5' }
+            ];
         }
 
     ];
