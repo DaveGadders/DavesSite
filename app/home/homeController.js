e3beaaf797521dde411b7c8069132ab2b2000594
Dave Gadsden <davegadsden92@googlemail.com>
2015-04-01 23:24:56 +0100

    Completed skills section on home page and fully responsive

--- app/home/homeController.js
+++ app/home/homeController.js
@@ -5,11 +5,11 @@ var homeController =
 
         function($scope) {
             $scope.skills = [
-                { skillname: 'HTML5', img: '/assets/img/html5.png', imagealt: 'HTML5', description: 'Description 1' },
-                { skillname: 'CSS3', img: '/assets/img/css.png', imagealt: 'CSS3', description: 'Description 2' },
-                { skillname: 'JavaScript', img: '/assets/img/js3.png', imagealt: 'JavaScript', description: 'Description 3' },
-                { skillname: 'AngularJS', img: '/assets/img/angularjs.png', imagealt: 'AngularJS', description: 'Description 4' },
-                { skillname: 'Ionic Mobile Hybrid Framework', img: '/assets/img/ionic_logo.png', imagealt: 'Ionic', description: 'Description 5' }
+                { skillname: 'HTML5', img: '/assets/img/skill1.png', imagealt: 'HTML5', description: 'Description 1' },
+                { skillname: 'CSS3', img: '/assets/img/skill2.png', imagealt: 'CSS3', description: 'Description 2' },
+                { skillname: 'JavaScript', img: '/assets/img/skill3.png', imagealt: 'JavaScript', description: 'Description 3' },
+                { skillname: 'AngularJS', img: '/assets/img/skill4.png', imagealt: 'AngularJS', description: 'Description 4' },
+                { skillname: 'Ionic Mobile Hybrid Framework', img: '/assets/img/skill5.png', imagealt: 'Ionic', description: 'Description 5' }
             ];
         }
 
